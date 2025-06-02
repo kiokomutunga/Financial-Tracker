8e64df60aa5d746723062d4552f095a1f0e53bca
kiokomutunga <kiokomutungapp@gmail.com>
2025-06-02 03:27:46 +0300

    models

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .models import Transactions
+from .forms import TransactionForm
 
-# Create your views here.
+
+def Dashboard (request):
+    transactions = Transactions.objects.all().order_by('-date')
+    total_income = sum(t.amount for t in transactions if t.type == 'income')
+    total_expense = sum(t.amount for t in transactions if t.type == 'expenses')
+    balance  = total_income - total_expense
+
+
+    return render (request, 'tracker/dashboard.html',{
+            'transactions':transactions,
+            'total_income':total_income,
+            'total_expense':total_expense,
+            'balance': balance
+
+        }
+    )
+
+def add_transaction(request):
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_Valid():
+            form.save()
+            return redirect('dashboard')
+        else:
+            form = TransactionForm()
+            return render(request, 'tracker/add_transaction.html', {'form': form})
+        
+      
+
