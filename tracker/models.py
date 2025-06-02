05abc89e7b72f5a9017ab892329c5d178ca9fd15
kiokomutunga <kiokomutungapp@gmail.com>
2025-06-03 01:11:01 +0300

    models budget

--- tracker/models.py
+++ tracker/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Transactions(models.Model):
 #choice for the type of transaction
@@ -17,6 +18,24 @@ class Transactions(models.Model):
     def __str__(self):
         return f"{self.Type} - {self.category} - {self.amount}"
 
+class Budget(models.Model):
+    CATEGORY_CHOICE = [
+        ('Food','Food'),
+        ('Transport','Transport'),
+        ('Housing','Housing'),
+        ('Entertainment','Entertainment'),
+        ('Other','Other'),
+    ]
+
+    #tables model and tables
+    Category = models.CharField(max_length=100,choices=CATEGORY_CHOICE)
+    amount = models.FloatField()
+    month = models.DateTimeField()
+    year = models.DateTimeField() 
+
+
+    def __str__(self):
+        return f"{self.category} - {self.month}/{self.year} - {self.amount}"
 
 
 
